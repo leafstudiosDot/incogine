90fae29be4c078b7eeecd8027d7d373e2e0073f7
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2024-10-06 21:21:47 +0800

    Initializing Splash Screen

--- src/scenes/splash/splash.cpp
+++ src/scenes/splash/splash.cpp
@@ -2,9 +2,24 @@
 #include "../../core/engine/engine.h"
 #include "../../scenes/MainScene.h"
 
+Splash::Splash() {
+    // Scene constructor
+    font.Init(Engine::Instance(0, nullptr)->GetRenderer());
+}
+
+Splash::~Splash() {
+    // Scene destructor
+}
+
 void Splash::Start() {
     // Scene initialized, calls at the initialization of the scene
-    Engine::Instance(0, nullptr)->SetScene(new MainScene());
+
+    font.setFont(reinterpret_cast<const char*>(_mainfont_data), _mainfont_size);
+    if (!font.GetFont()) {
+        std::cerr << "Failed to load font in Splash::Start" << std::endl;
+    }
+
+    //Engine::Instance(0, nullptr)->SetScene(new MainScene());
 }
 
 void Splash::Update() {
@@ -13,4 +28,5 @@ void Splash::Update() {
 
 void Splash::Render() {
     // Scene render
+    font.renderUI("Splash Scene", 10, 10, 24);
 }
--- src/scenes/splash/splash.h
+++ src/scenes/splash/splash.h
@@ -1,15 +1,20 @@
 #include <iostream>
 #include "../../core/scenes/scenes.h"
+#include "../../core/fonts/fonts.h"
+#include "../../fonts/main_font.h"
 
 #ifndef SPLASH_H
 #define SPLASH_H
 
 class Splash : public Scene {
+    private:
+        Font font;
     public:
+        Splash();
+        ~Splash();
+
         void Start() override;
-
         void Update() override;
-
         void Render() override;
 };
 
