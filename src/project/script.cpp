cb60bb78b222bb623008ca8f0dacc820000cc90d
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2025-03-04 22:57:53 +0800

    add: Puroko Target
    Project Files and Scripts belongs to that Target

---
+++ src/project/script.cpp
@@ -0,0 +1,18 @@
+#include "script.h"
+#include <iostream>
+
+Script::Script() {
+    
+}
+
+Script::~Script() {
+    
+}
+
+void Script::Start() {
+    
+}
+
+void Script::Update() {
+    
+}
---
+++ src/project/script.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#ifndef PRSCRIPT_H
+#define PRSCRIPT_H
+
+class Script {
+    public:
+        Script();
+        ~Script();
+
+        void Start();
+        void Update();
+};
+
+#endif
