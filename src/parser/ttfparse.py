538a5b97116d03b3ee3f5514dbe5b4d8dc3c2bd4
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2025-05-12 20:50:14 +0800

    patch: JPSUP Font problem
    CMake and Python problem, this patch will fix the font conversion and something.

--- src/parser/ttfparse.py
+++ src/parser/ttfparse.py
@@ -1,25 +1,26 @@
 import sys
 
-def write_header(data, filename):
+def write_header(data, filename, headername):
     with open(filename, 'w') as f:
-        f.write("#ifndef FONT_H\n")
-        f.write("#define FONT_H\n\n")
-        f.write("const unsigned char " + sys.argv[1].split(".")[0] +"_font_data[] = {")
+        f.write("#ifndef "+ headername.upper() +"_FONT_H\n")
+        f.write("#define "+ headername.upper() +"_FONT_H\n\n")
+        f.write("const unsigned char " + headername + "_font_data[] = {")
         for i, byte in enumerate(data):
             f.write(hex(byte) + ",")
             if (i + 1) % 12 == 0:
                 f.write("\n")
         f.write("\n};\n")
-        f.write("const unsigned int " + sys.argv[1].split(".")[0] +"_font_size = {};\n\n".format(len(data)))
+        f.write("const unsigned int " + headername + "_font_size = {};\n\n".format(len(data)))
         f.write("#endif // FONT_H\n")
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: {} <input.ttf> <output.h>".format(sys.argv[0]))
+    if len(sys.argv) != 4:
+        print("Usage: {} <input.ttf> <output.h> <headername>".format(sys.argv[0]))
         sys.exit(1)
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
+    headername = sys.argv[3]
 
     try:
         with open(input_file, 'rb') as f:
@@ -28,7 +29,7 @@ def main():
         print("Error opening input file.")
         sys.exit(1)
 
-    write_header(data, output_file)
+    write_header(data, output_file, headername)
 
 if __name__ == "__main__":
     main()
