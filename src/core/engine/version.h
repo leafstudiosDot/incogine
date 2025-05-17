7cf98009f82f734f68d9cbbff90be1139ebb80d2
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2025-05-17 11:48:59 +0800

    update: Version v0.0.0.2
    Ikuyo! It's been a year Ig

--- src/core/engine/version.h
+++ src/core/engine/version.h
@@ -9,8 +9,8 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 1
+#define VERSION_BUILD 2
 
-#define VERSION_STRING "0.0.0.1"
+#define VERSION_STRING "0.0.0.2"
 
 #endif // VERSION_H
