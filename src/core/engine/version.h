c965951cbbd855eba7dd6592429a22f3404ffdb6
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2026-03-05 17:06:25 +0800

    update: Version 0.0.0.3
    This will be the next major version of Incogine

--- src/core/engine/version.h
+++ src/core/engine/version.h
@@ -9,8 +9,8 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 2
+#define VERSION_BUILD 3
 
-#define VERSION_STRING "0.0.0.2"
+#define VERSION_STRING "0.0.0.3"
 
 #endif // VERSION_H
